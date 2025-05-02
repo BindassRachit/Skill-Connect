fb182f507bf93b059888d7eff91592039727f91a
Rachit Gupta <rachitgupta98977@gmail.com>
2025-05-02 17:12:08 +0530

    Updated project

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const toggleBtn = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-
-toggleBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
-});
