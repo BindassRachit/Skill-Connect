a51c3f8c208d78a709c45c9ed02967a0028ae568
Rachit Gupta <rachitgupta98977@gmail.com>
2025-05-03 13:01:52 +0530

    Initial commit: added navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/home/homepage';
+// In your main JS file
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,3 +13,8 @@ root.render(
     <HomePage />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
