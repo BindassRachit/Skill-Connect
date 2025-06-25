44ada19021da0ed8bde6eeb752751c857bf2288c
Rachit Gupta <rachitgupta98977@gmail.com>
2025-06-25 15:40:48 +0530

    find a job

---
+++ frontend/src/landing_page/find_a_job/find_a_job.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="find-a-job">
+      <h1>Find a Job</h1>
+    </div>
+  );
+};
+
+
+
+export default App;
